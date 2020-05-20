a659e3711671ad8fb600693b6a9722ba45a5e645
KeSHaMI <61086387+KeSHaMI@users.noreply.github.com>
2020-05-20 16:59:05 +0300

    Finished bot???

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 import json
 import requests
 import re
-from config import Token, password
+from config import Token, password, domen, login, password_box, method
 import pymongo
 from collections import Counter
 
@@ -18,8 +18,10 @@ collection_user = db['Users']
 
 
 def get_user(call, key):
-    return  list(collection_user.find({'_id' : call.message.chat.id}))[0][key]
-
+    try:
+        return  list(collection_user.find({'_id' : call.message.chat.id}))[0][key]
+    except:
+        return list(collection_user.find({'_id': call.chat.id}))[0][key]
 def get_tovar(call, key, index='index'):
 
     tovar = list(collection.find({'category': get_user(call, 'category')}))[int(get_user(call, index))]
@@ -33,12 +35,13 @@ def get_bucket(call):
 def start(message):
     if not collection_user.find_one({'_id': message.chat.id}):
         collection_user.insert_one({'_id': message.chat.id,
-                                    'name': message.from_user.first_name,
+                                    'name': None,
                                     'number': 0,
                                     'index': 0,
                                     'index_buck': 0,
                                     'category': 0,
                                     'tovar_id': [],
+
                                     })
     else:
 
@@ -46,17 +49,20 @@ def start(message):
                                    {'$set' : {'index' : 0,
                                               'category' : 0,
                                               'index_buck': 0,
-                                              'tovar_id' : [],}})
+                                              }})
 
     markup = types.InlineKeyboardMarkup(row_width=1)
 
     but1 = types.InlineKeyboardButton('Товари', callback_data='tovars')
     but2 = types.InlineKeyboardButton('Кошик', callback_data='bucket')
-    but3 = types.InlineKeyboardButton('Замовлення', callback_data='orders')
+    but3 = types.InlineKeyboardButton('Історія замовлень', callback_data='orders')
 
     markup.add(but1, but2, but3)
 
-    bot.send_message(message.chat.id, 'Вас вітає телеграм-магазин Yumi!\n'
+    bot.send_message(message.chat.id, 'Вітаємо!\n'
+                                      'Я бот для замовлень продукції YUMI Lashes \n' 
+                                      'Виберіть товари та добавте їх в кошик. \n'
+                                      'Після замовлення менеджер звяжеться з вами для відправки товару.\n'
                                       'Оберіть потрібну дію', reply_markup=markup)
 
 
@@ -78,7 +84,7 @@ def tovars(call):
     if  call.data in ['13', '14']:
         collection_user.update_one({'_id':call.message.chat.id}, {'$set': {'category': call.data}})
 
-        markup.row(types.InlineKeyboardButton('{} грн'.format(get_tovar(call, 'price')[:-3]),
+        markup.row(types.InlineKeyboardButton('Замовити: ({} грн)'.format(get_tovar(call, 'price')[:-3]),
                                           callback_data='add{}'.format(get_tovar(call, '_id'))))
 
 
@@ -90,19 +96,26 @@ def tovars(call):
 
         markup.row(but1, but2, but3)
 
-        bot.send_message(call.message.chat.id,
-                         '<a href="{1}">{0}</a>'.format(get_tovar(call, 'name'), get_tovar(call, 'image')), parse_mode='HTML', reply_markup=markup)
+        markup.row(types.InlineKeyboardButton('Кошик', callback_data='bucket'))
+
+        markup.row(types.InlineKeyboardButton('Повернутися назад', callback_data='tovars'))
+
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='<a href="{1}">{0}</a>'.format(get_tovar(call, 'name'), get_tovar(call, 'image')),
+                              parse_mode='HTML', reply_markup=markup
+                              )
 
     else:
         if Counter(get_user(call, 'tovar_id')).get(get_tovar(call, '_id')):
-            markup.row(types.InlineKeyboardButton('{} грн - {}шт'.format(get_tovar(call, 'price')[:-3],
+            markup.row(types.InlineKeyboardButton('Замовити: ({} грн) - {}шт'.format(get_tovar(call, 'price')[:-3],
                                                     Counter(get_user(call, 'tovar_id')).get(get_tovar(call, '_id'))),
                                                   callback_data='add{}'.format(get_tovar(call, '_id'))))
 
-            markup.row(types.InlineKeyboardButton('Кошик', callback_data='bucket'))
+
         else:
             markup.row(types.InlineKeyboardButton('{} грн'.format(get_tovar(call, 'price')[:-3]),
                                                   callback_data='add{}'.format(get_tovar(call, '_id'))))
+
 
         but1 = types.InlineKeyboardButton('⬅', callback_data='minus')
         but2 = types.InlineKeyboardButton('{}/{}'.format(int(get_user(call, 'index')) + 1,
@@ -112,12 +125,13 @@ def tovars(call):
         but3 = types.InlineKeyboardButton('➡', callback_data='plus')
 
         markup.row(but1, but2, but3)
+        markup.row(types.InlineKeyboardButton('Кошик', callback_data='bucket'))
+        markup.row(types.InlineKeyboardButton('Повернутися назад', callback_data='tovars'))
 
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup,
                               text='<a href="{1}">{0}</a>'.format(get_tovar(call, 'name'), get_tovar(call, 'image')), parse_mode='HTML'
                               )
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      reply_markup=markup)
+
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('ad'))
 def add_tovar(call):
@@ -184,10 +198,9 @@ def bucket_upd(call):
                               text='Ваш кошик:\n'
                                     '<a href="{1}">{0}</a>'.format(list(collection.find({'_id': get_bucket(call)}))[0]['name'],
                                                                    list(collection.find({'_id': get_bucket(call)}))[0]['image']),
-                              parse_mode='HTML'
+                              parse_mode='HTML', reply_markup=markup
                               )
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      reply_markup=markup)
+
     except IndexError:
         bot.send_message(call.message.chat.id, 'В кошику немає товарів :(')
         categories(call)
@@ -218,6 +231,191 @@ def delete(call):
 
 
     bucket_upd(call)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'confirm')
+def confirm(call):
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    but1 = types.InlineKeyboardButton('Самовивіз', callback_data='Самовивіз')
+    but2 = types.InlineKeyboardButton('Нова Пошта', callback_data='Нова Пошта')
+    but3 = types.InlineKeyboardButton('Відмінити замовлення', callback_data='cancel')
+    but4 = types.InlineKeyboardButton('Повернутись', callback_data='bucket')
+
+    markup.add(but1, but2, but3, but4)
+
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          text='Виберіть спосіб доставки', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data in ['Самовивіз', 'Нова Пошта'])
+def record_delivery(call):
+    collection_user.update_one({'_id': call.message.chat.id},
+                               {'$set' : {'delivery' : call.data.replace('Нова Пошта', '2').replace('Самовивіз', '1')}})
+    if get_user(call, 'name') == None:
+        get_name(call.message)
+    else:
+        summary(call.message)
+
+
+def get_name(message):
+    bot.send_message(message.chat.id, 'Введіть ваше ім\'я:')
+    bot.register_next_step_handler(message, record_name)
+
+def record_name(message):
+    collection_user.update_one({'_id': message.chat.id},
+                               {'$set': {'name': message.text.replace(' ', '%20')}})
+    if get_user(message, 'number') != 0:
+        summary(message)
+    else:
+        get_number(message)
+
+def get_number(message):
+
+    markup = types.ReplyKeyboardMarkup()
+
+    but1 = types.KeyboardButton('Відправити контакт', request_contact=True)
+
+    markup.add(but1)
+
+    bot.send_message(message.chat.id, ' Введіть ваш номер телефону в форматі (380) або відправте контакт',
+                     reply_markup=markup)
+    bot.register_next_step_handler(message, record_number)
+
+def record_number(message):
+    if type(message.contact) == type(None):  # Protection vs non-contact input
+        if re.match('^380\d{9}$', message.text):  # Cheking if input is a phone number
+            collection_user.update_one({'_id': message.chat.id},
+                                       {'$set': {'number': message.text}})
+            summary(message)
+        else:
+            bot.send_message(chat_id=message.chat.id, text='Номер в неправильному форматі!')
+            get_number(message)
+
+    else:
+        collection_user.update_one({'_id': message.chat.id},
+                                   {'$set': {'number': message.contact.phone_number[1:]}})
+        summary(message)
+
+def summary(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    but1 = types.InlineKeyboardButton('Змінити спосіб доставки', callback_data='confirm')
+    but2 = types.InlineKeyboardButton('Змінити ім\'я', callback_data='get_name')
+    but3 = types.InlineKeyboardButton('Змінити номер телефону', callback_data='get_number')
+    but4 = types.InlineKeyboardButton('Підтвердити замовлення', callback_data='final')
+
+    markup.add(but1, but2, but3, but4)
+
+    bot.send_message(chat_id=message.chat.id, reply_markup=markup,
+                          text="Дані замовлення:\n"
+                               "Сумма замовлення: _{0}грн_\n"
+                               "Ім'я: _{1}_\n"
+                               "Телефон: _{2}_\n"
+                               "Спосіб доставки: _{3}_".format(
+                              sum([int(list(collection.find({'_id' : key}))[0]['price'][:-3])*value for key, value in Counter(get_user(message, 'tovar_id')).items()]),
+                              get_user(message, 'name'), get_user(message, 'number'), 'Нова Пошта' if get_user(message, 'delivery') == 2 else 'Самовивіз'
+
+
+                          ), parse_mode='MarkdownV2')
+
+@bot.callback_query_handler(func=lambda call: call.data in ['get_name', 'get_number'])
+def change(call):
+    if call.data == 'get_number':
+        get_number(call.message)
+    else:
+        get_name(call.message)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'final')
+def send_request(call):
+
+    clientnamefirst = get_user(call, 'name')
+    clientphone =  get_user(call, 'number')
+    delivery = get_user(call, 'delivery')
+
+    url = '{0}{1}?login={2}' \
+                      '&password={3}' \
+                      '&workflowid=11' \
+                      '&clientnamefirst={4}' \
+                      '&clientphone={5}' \
+                      '&order_deliveryid={6}'.format(domen, method, login, password_box,
+                                                clientnamefirst,
+                                                clientphone, delivery)
+
+    for i, item in enumerate(Counter(get_user(call, 'tovar_id')).most_common()):
+        url += '&productArray[{0}][id]={2}&productArray[{1}][count]={3}'.format(
+            i, i, item[0], item[1]
+        )
+    r_update = requests.get(url=url)
+
+    data = r_update.json()
+    if data['result'] == 'ok':
+        bot.send_message(call.message.chat.id, 'Заказ успешно создан')
+        collection_user.update_one({'_id': call.message.chat.id},
+                                   {'$set': {'index': 0,
+                                             'category': 0,
+                                             'index_buck': 0,
+                                             'tovar_id' : [],
+                                             'delivery' : None
+                                             }})
+    start(call.message)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'orders')
+def get_ord_number(call):
+    if get_user(call, 'number') == 0:
+        markup = types.ReplyKeyboardMarkup()
+
+        but1 = types.KeyboardButton('Відправити контакт', request_contact=True)
+
+        markup.add(but1)
+
+        bot.send_message(call.message.chat.id, ' Введіть ваш номер телефону в форматі (380) або відправте контакт',
+                         reply_markup=markup)
+        bot.register_next_step_handler(call.message, ord_record_number)
+    else:
+        orders(call.message)
+
+def ord_record_number(message):
+    if type(message.contact) == type(None):  # Protection vs non-contact input
+        if re.match('^380\d{9}$', message.text):  # Cheking if input is a phone number
+            collection_user.update_one({'_id': message.chat.id},
+                                       {'$set': {'number': message.text}})
+            orders(message)
+        else:
+            bot.send_message(chat_id=message.chat.id, text='Номер в неправильному форматі!')
+            get_ord_number(message)
+
+    else:
+        collection_user.update_one({'_id': message.chat.id},
+                                   {'$set': {'number': message.contact.phone_number[1:]}})
+        orders(message)
+
+def orders(message):
+    number = get_user(message, 'number')
+
+    url = 'https://crm.yumilashes.com.ua/api/orders/get/'\
+        f'?login=yuriy&password=5cd1e067f9a747c45e6d5a0896aac817&clientphone={number}&workflowid=11'
+
+    data = requests.get(url).json()
+
+    for i in range(len(data['orders'])):
+        text = ''
+        text += 'Замовлення № ' + data['orders'][i]['orderid'] + '\n'
+        text += 'Ваші товари:' + '\n'
+        for x in range(len(data['orders'][i]['products'])):
+            text += data['orders'][i]['products'][x]['name'] + ' - ' + data['orders'][i]['products'][x]['count'][:-4] + '\n'
+        text += 'Сума: '  + data['orders'][i]['sum'][:-3] + '\n'
+        text += 'Статус замовлення:' + data['orders'][i]['statusname']
+        bot.send_message(message.chat.id, text)
+
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    but1 = types.InlineKeyboardButton('Товари', callback_data='tovars')
+    but2 = types.InlineKeyboardButton('Кошик', callback_data='bucket')
+    but3 = types.InlineKeyboardButton('Замовлення', callback_data='orders')
+
+    markup.add(but1, but2, but3)
+
+    bot.send_message(message.chat.id, 'Оберіть потрібну дію', reply_markup=markup, )
 
 
 
